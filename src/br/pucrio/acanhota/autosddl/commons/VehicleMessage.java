43c0e82e7ffc0b6fa8e34b51f29cf92700366e15
antoniocanhota <antoniocanhota@gmail.com>
2015-11-28 19:20:35 -0200

    Added location, speed and rpm information

--- src/br/pucrio/acanhota/autosddl/commons/VehicleMessage.java
+++ src/br/pucrio/acanhota/autosddl/commons/VehicleMessage.java
@@ -12,6 +12,10 @@ public class VehicleMessage implements Serializable {
 	private int m_state;
 	private Date createdAt;
 	private VehicleMessageType type;
+	private double lat;
+	private double lng;
+	private int	vehicleSpeed;
+	private int	rpm;
 	private boolean obd2Connected;
 	
 	public VehicleMessage(VehicleMessageType type, boolean obd2Connected)
@@ -29,10 +33,12 @@ public class VehicleMessage implements Serializable {
 		sb.append("createdAt = '" + getCreatedAtAsStr() + "'").append(", ");
 		sb.append("type = " + type + "").append(", ");
 		sb.append("obd2Connected = " + obd2Connected + "").append(", ");
+		sb.append("coordinates = " + getCoordinatesAsStr() + "").append(", ");
+		sb.append("vehicleSpeed (km/h) = " + vehicleSpeed + "").append(", ");
+		sb.append("rpm = " + getRpm() + "").append(", ");
 		sb.append("m_state = '" + (m_state == 0 ? "Ping!" : "Pong!") + "'");
 		sb.append("]");
 		return sb.toString();
-
 	}
 	
 	public void changeState()
@@ -50,5 +56,41 @@ public class VehicleMessage implements Serializable {
 	public String getCreatedAtAsStr() {
 		DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		return df.format(createdAt);
+	}
+
+	public int getSpeed() {
+		return vehicleSpeed;
+	}
+
+	public void setSpeed(int speed) {
+		this.vehicleSpeed = speed;
+	}
+
+	public int getRpm() {
+		return rpm;
+	}
+
+	public void setRpm(int rpm) {
+		this.rpm = rpm;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLng() {
+		return lng;
+	}
+
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+	
+	public String getCoordinatesAsStr() {
+		return "("+lat + "," + lng+")";
 	}
 }
